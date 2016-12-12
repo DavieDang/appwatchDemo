9f499e403926f0a52112938782ecffc30ca596ef
dangwc <dangwc@gz-mstc.com>
2016-12-12 14:35:16 +0800

    Initial Commit

---
+++ appwatchDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  appwatchDemo
+//
+//  Created by admin on 2016/12/12.
+//  Copyright © 2016年 gzpingao.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
